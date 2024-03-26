7b5579279b53d38b2e544636cd062170e474bcc6
lackhead <clake@lackhead.org>
2024-03-25 22:10:32 -0700

    Overhaul of flow- loops, formatting, update of cleanup

--- homebrew/config_homebrew.sh
+++ homebrew/config_homebrew.sh
@@ -7,6 +7,10 @@
 
 set -e
 
+# find out how to output bold
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 # Check for Mac
 if test "$(uname)" = "Darwin"
 then
@@ -14,53 +18,38 @@ then
    # Check for Homebrew
    if test ! $(which brew); then
       echo "Installing Homebrew..."
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-   else
-      echo "Homebrew already installed at" $(which brew)
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
    fi
 
    # Make sure we're up to date
-   echo "Updating all brews..."
-   brew upgrade
+   brew update
 
    # Add some casks
    brew tap "homebrew/bundle"
 
    # Applications
-   echo "Installing applications..."
-   brew install --cask --appdir="/Applications" vlc
-   brew install --cask --appdir="/Applications" jumpcut
-   brew install --cask --appdir="/Applications" skim
-   brew install --cask --appdir="/Applications" spotify
-   brew install --cask --appdir="/Applications" sublime-text
-   brew install --cask --appdir="/Applications" vlc
-   brew install --cask --appdir="/Applications" iterm2
+   for app in vlc jumpcut skim spotify sublime-text iterm2; do
+      if $(brew list ${app} &>/dev/null); then
+         echo "${bold}${app}${normal} already installed"
+      else
+         brew install --cask --appdir="/Applications" ${app}
+      fi
+   done
 
    # Utilities
-   echo "Installing utilities..."
-   brew install "ack"
-   brew install "coreutils"
-   brew install "curl"
-   brew install "dict"
-   brew install "git"
-   brew install "gettext"
-   brew install "git"
-   brew install "gnupg"
-   brew install "imagemagick"
-   brew install "mplayer"
-   brew install "ncftp"
-   brew install "nmap"
-   brew install "pwgen"
-   brew install "rcs"
-   brew install "telnet"
-   brew install "rar"
-   brew install "wget"
-   brew install "youtube-dl"
-   brew install "vivid" 
+   for util in ack coreutils curl dict git gettext gnupg imagemagick mplayer ncftp nmap pwgen rcs telnet rar wget youtube-dl vivid; do
+      if $(brew list ${util} &>/dev/null); then
+         echo "${bold}${util}${normal} already installed"
+      else
+         brew install ${util}
+      fi
+   done
+
+   # Upgrades
+   brew upgrade
 
    # Cleanup
-   brew cleanup
-   /bin/rm -f -r /Library/Caches/Homebrew/*
+   brew cleanup -s
 
 else
    echo "I only install Homebrew on Mac systems"
